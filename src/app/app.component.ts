7b94c541d6c423d64bca8ca71629ad5823cff1f2
Brayan Valerio <bavaleriom@gmail.com>
2024-10-08 14:45:54 -0600

    Components using signals and single service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../service/investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,11 +11,26 @@ import { FormsModule } from '@angular/forms';
 })
 export class UserComponent {
 
-  enteredInitialInvestment = '0';
+  constructor(public investmentService: InvestmentService) {}
+
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
   
 
   onSubmit() {
-    console.log('Submitted!');
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+    });
+
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 
 }
